cff8f9000edb9dabe9d316c0f090234487d4fb09
TakashiVásquez <takashivm97@gmail.com>
2022-07-14 18:26:14 -0500

    CRUD V1

---
+++ Mestro_TipoCaracteristicas/Program.cs
@@ -0,0 +1,68 @@
+
+using Maestro_TipoCaracteristicas.Repositories;
+using Maestro_TipoCaracteristicas.Contracts;
+using Maestro_TipoCaracteristicas.Helpers;
+using Maestro_TipoCaracteristicas.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration(config =>
+    {
+        config
+            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+    })
+    .ConfigureServices(services =>
+    {
+        var config = new ConfigurationBuilder()
+           .SetBasePath(Path.Combine(Directory.GetCurrentDirectory()))
+           .AddJsonFile("local.settings.json", true)
+           .AddEnvironmentVariables()
+           .Build();
+        // Database connection service
+        //var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings:DefaultConnection");
+
+        services.AddDbContext<ApiDataContext>(options =>
+            options.UseSqlServer(
+                 config["ConnectionStrings:DefaultConnection"],
+                sqlServerOptions => sqlServerOptions.CommandTimeout(600)));
+        #region JWT Authentication
+        // JWT Authentication
+        //services.AddAuthentication(opt =>
+        //{
+        //    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        //    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        //})
+        //    .AddJwtBearer(options =>
+        //    {
+        //        options.TokenValidationParameters = new TokenValidationParameters
+        //        {
+        //            ValidateIssuer = true,
+        //            ValidateAudience = true,
+        //            ValidateLifetime = true,
+        //            ValidateIssuerSigningKey = true,
+        //            ValidIssuer = "https://localhost:7071",
+        //            ValidAudience = "https://localhost:7071",
+        //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("fjurkbdlhmklqacwqzdxmkkhvqoclyqz"))
+        //        };
+        //    });
+        #endregion
+
+
+        // DI for customized services and repositories
+        services.AddScoped<ITipoRepository, TipoCRepository>();
+        //services.AddTransient<ITipoRepository, TipoCRepository>();  
+        services.AddScoped<ITipoService, TipoCServices>();
+        //services.AddTransient<ITipoService, TipoCServices>();
+
+    })
+    .UseDefaultServiceProvider(options => options.ValidateScopes = false)
+    .Build();
+
+host.Run();
